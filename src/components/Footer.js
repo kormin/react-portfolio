11f3adaec2294438937c4b1d478d3e0f791b5c28
Tom <abaotom14@gmail.com>
2019-06-27 20:44:34 +0800

    add bootstrap and react-bootstrap dependencies. add Header and Foooter

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      // Footer
+      <footer class="text-center">Developed by Tom Abao &copy; 2019</footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      // Navigation
+      <div class="container">
+        <nav class="nav">
+          <a class="nav-link" href="#">
+            Kormin
+          </a>
+          <a class="nav-link" href="#about">
+            About
+          </a>
+          <a class="nav-link" href="#interests">
+            Interests
+          </a>
+          <a class="nav-link" href="#skills">
+            Skills
+          </a>
+          <a class="nav-link" href="#contact">
+            Contact
+          </a>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Header;
