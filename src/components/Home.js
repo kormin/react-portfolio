acfa17eaf69384445bbde1b11d25df91128aec43
Tom <abaotom14@gmail.com>
2020-05-30 08:49:37 +0800

    add about me

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,6 +18,17 @@ class Home extends Component {
           Web Developer
           <br />
           Currently building with Python
+          <br />
+          <div className="terminal-nano">
+            tom@desktop:~$ nano about-me.txt
+            <br />
+            <span>
+              I play computer games, read books, and watch movies and television shows.
+              I enjoy the Pokemon games series.
+              I've read all the Harry Potter books.
+              I'm also interested in fitness.
+            </span>
+          </div>
         </div>
 
       </div>
