8f984c4b0c65ada6e65aacf7f998f4fe1aaf6e60
Tom <abaotom14@gmail.com>
2020-06-06 08:32:18 +0800

    add menu items

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,53 @@
 import React, { Component } from 'react';
 
+const menuItems = [
+  {
+    'name': '>tom_',
+    'link': '#home'
+  },
+  {
+    'name': 'about',
+    'link': '#about'
+  },
+  {
+    'name': 'work',
+    'link': '#work'
+  },
+  {
+    'name': 'contact',
+    'link': '#contact'
+  }
+];
+
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      active: menuItems[0].name
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(name) { 
+    this.setState({ active: name });
+  }
+
   render() {
     return (
       // Navigation
       <div id="header" className="row">
         <ul className="taskbar">
-          <li className="active">
-            <a href="#home">
-              >tom_
-            </a>
-          </li>
-          <li>
-            <a href="#about">
-              about
-            </a>
-          </li>
-          <li>
-            <a href="#work">
-              work
-            </a>
-          </li>
-          <li>
-            <a href="#contact">
-              contact
-            </a>
-          </li>
+          {menuItems.map((menuItem, i) =>
+            <li
+              key={i}
+              className={this.state.active === menuItem.name ? 'active' : ''} 
+              onClick={this.handleClick.bind(this, menuItem.name)}
+            >
+              <a href={menuItem.link}>
+                {menuItem.name}
+              </a>
+            </li>
+          )}
         </ul>
       </div>
     );
