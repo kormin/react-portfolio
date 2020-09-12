954a7b2d4119f76f41dfd1254ed092134e673aa0
Tom <abaotom14@gmail.com>
2020-09-12 15:15:41 +0800

    Updates

---
+++ precache-manifest.970df173b3bc49af2350c9ed1aebe35c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73686cff2a43f4419ff54df8a04fb37c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98214b3324fa5a4c5ddf",
+    "url": "/static/css/main.7042cddd.chunk.css"
+  },
+  {
+    "revision": "ecf9bf2ce24fb738692b",
+    "url": "/static/js/2.2760ad10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2760ad10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98214b3324fa5a4c5ddf",
+    "url": "/static/js/main.7c10e915.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "3a7c47cbda46dffafac595162b85742f",
+    "url": "/static/media/Roboto-Regular-webfont.3a7c47cb.ttf"
+  },
+  {
+    "revision": "b3be96cbbfd0ff1f3ba3c7c2f58c1441",
+    "url": "/static/media/Roboto-Regular-webfont.b3be96cb.woff"
+  }
+]);
